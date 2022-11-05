0e423cdd73e34e90ecb4cc32804d7abfbc1261a9
paulo moreira <a64459@alunos.uminho.pt>
2022-11-05 16:44:49 +0000

    criei tipodeobs e documentei o que havia criado

--- src/Tarefa1_2022li1g088.hs
+++ src/Tarefa1_2022li1g088.hs
@@ -14,15 +14,24 @@ mapaValido :: Mapa -> Bool
 mapaValido = undefined
 
 
-{-mapaValido Mapa larg ((Relva), (x:xy))
-  | x == Carro || x== Tronco = False
-  | othewise = mapaValido xs
-   larg == length (x:xy) -}
+{-|Funcao que verifica que tem algum espaço com Nenhum obstaculo-}
 vervazios :: [Obstaculo] -> Bool
 vervazios [] = False
 vervazios (x:xs) 
   | x== Nenhum = True
   |otherwise = vervazios xs
-
+{-|funcao que valida que a largura é do tamanho da lista de obstaculos-}
 vernrobstaculos :: Mapa -> Bool
 vernrobstaculos (Mapa l [(_ , k)]) = l == length k
+{-|funcao que verifica se o Terreno tem algum Obstaculo nao permitido-}
+tipodeobs :: Mapa -> Bool
+tipodeobs (Mapa larg ([(terr, [])])) = True
+tipodeobs (Mapa larg ([(Relva, (x:xs))]))
+  | x == Carro || x== Tronco = False
+  | otherwise = tipodeobs (Mapa larg ([(Relva, (xs))]))
+tipodeobs (Mapa larg ([(Rio vel, (x:xs))]))
+  | x == Carro || x== Arvore = False
+  | otherwise = tipodeobs (Mapa larg ([(Rio vel, (xs))]))
+tipodeobs (Mapa larg ([(Estrada vel, (x:xs))]))
+  | x == Arvore || x== Tronco = False
+  | otherwise = tipodeobs (Mapa larg ([(Estrada vel, (xs))]))
