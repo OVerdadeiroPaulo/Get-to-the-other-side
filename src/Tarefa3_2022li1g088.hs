a9319550e785cad272c8d58334dc37ca6803fe1a
Silverio <A101536@alunos.uminho.pt>
2022-11-08 14:16:30 +0000

    resol 3

--- src/Tarefa3_2022li1g088.hs
+++ src/Tarefa3_2022li1g088.hs
@@ -9,7 +9,7 @@ Módulo para a realização da Tarefa 3 do projeto de LI1 em 2022/23.
 module Tarefa3_2022li1g088 where
 
 import LI12223
-import Tarefa1_2022li1g088 (vernrobstaculos)
+import Tarefa1_2022li1g088 
 
 animaJogo :: Jogo -> Jogada -> Jogo
 animaJogo = undefined
@@ -18,11 +18,24 @@ animaJogo = undefined
 deslocaria :: Jogador -> Jogada -> Coordenadas
 deslocaria (Jogador coords) jogada 
   | jogada == Parado = coords
+  | jogada == Move Cima && snd coords == 0 = coords
   | jogada == Move Cima = (fst coords, (snd coords-1))
   | jogada == Move Baixo = (fst coords, (snd coords+1))
   | jogada == Move Esquerda = ((fst coords -1), (snd coords))
   | jogada == Move Direita = ((fst coords +1), (snd coords))
+  
+
 animacoords :: Coordenadas -> Coordenadas
 animacoords coords = (fst coords, (snd coords-1))
 veobstaculonacoordenada :: Mapa -> Obstaculo
 veobstaculonacoordenada (Mapa l (((terr, (x:xs)):ys)))
+
+positionobs :: Mapa -> Coordenadas
+positionobs (Mapa la ((te,(x:xs)):ys)) = (elemIndices (head ((te,(x:xs)):ys)) ((te,(x:xs)):ys), elemIndeces x (x:xs))
+
+
+{-
+limitemapa :: Jogador -> Jogada -> Coordenadas
+limitemapa (Jogador coords) jogada | coords == (0,_) && Jogada == Move Cima = (0,_)
+                                   | Jogador   
+-}
