df1e058c8d36850b664fb3061008ad3bdb5f84c9
paulo moreira <a64459@alunos.uminho.pt>
2022-11-08 16:17:58 +0000

    corrigido mais alguns

--- src/Tarefa2_2022li1g088.hs
+++ src/Tarefa2_2022li1g088.hs
@@ -11,6 +11,7 @@ module Tarefa2_2022li1g088 where
 import LI12223 
 import Tarefa1_2022li1g088
 import System.Random
+import LI12223 (Mapa)
 
 
 
@@ -19,7 +20,7 @@ estendeMapa (Mapa la linhas@(x:xs)) | terreno == Rio 0 = (Mapa la ((Rio ve, obst
                                     | terreno == Estrada 0 = (Mapa la ((Estrada ve, obstaculos):xs))
                                     | terreno == Relva = (Mapa la ((relva, obstaculos):xs))
             where obstaculos = proximosObstaculosValidos la  x 
-                  terreno = proximosTerrenosValidos (Mapa la linhas @(x:xs))
+--                  terreno = proximosTerrenosValidos (Mapa la linhas @(x:xs))
                   ve = (head randomIntsL)
 
 randomIntsL :: Int -> Int -> [Int]
@@ -33,11 +34,31 @@ proximosTerrenosValidos (Mapa _ ((te,obs):xs))| isRio (Mapa _ ((te,obs):xs)) = [
                                               | isRelva (Mapa _ ((te,obs):xs)) = [Estrada 0, Rio 0]
                                               | otherwise = [Rio 0, Estrada 0, Relva]
 
+
+{-auxiliar para silverio-}
+terrenoscontiguos2 :: Mapa -> Bool
+terrenoscontiguos2 (Mapa _ (([]))) = False
+terrenoscontiguos2 mapa@(Mapa l (((x):xs)))
+  | inicio (fst(head (head (agrupaterrenos mapa)))) == "Est" && length (head (agrupaterrenos mapa)) >5 || inicio ( (head (agrupaterrenos mapa))) == "Rel" && length (head (agrupaterrenos mapa))  > 5 = True
+  | inicio (fst(head a)) == "Rio" && length (head (agrupaterrenos mapa)) > 4 = True
+  | otherwise = terrenoscontiguos2 (Mapa l (((xs))))
+      where (a:b) = agrupaterrenos mapa
+
+
+isrio3 :: Mapa -> Bool
+isrio3 mapa = isRio2 0 mapa
+isRio2 :: Int -> Mapa -> Bool
+isRio2 _ (Mapa _ []) = False
+isRio2 4 (Mapa _ ((te,obs):xs)) = True 
+isRio2 k (Mapa l ((te,obs):xs)) 
+  | inicio te == "Rio" = isRio2 (k+1)  (Mapa l ((xs)))
+{-
+
 {-Funcao que verifica se temos 4 rios para a main proximosTerrenosValidos-}
 isRio :: Int -> Mapa -> Bool
 isRio 0 (Mapa _ _) = True
 isRio _ (Mapa _ []) = False
-isRio 4 (Mapa _ ((te,obs):xs)) = case te of 
+isRio 4 (Mapa _ ((te,obs):xs)) =  case te of 
                                          (Rio _) -> isRio (n-1) xs
                                          _ -> False
 
@@ -54,14 +75,14 @@ isRelva :: Int -> Mapa -> Bool
 isRelva 0 (Mapa _ _) = True
 isRelva _ (Mapa _ []) = False
 isRelva 5 (Mapa _ ((te,obs):xs)) = case te of 
-                                           (Relva _) -> isRelva (n-1) xs
+                                           (Relva ) -> isRelva (n-1) xs
                                            _ -> False
-
+-}
 {-Funcao que verifica os possiveis proximos obstaculos validos-}
 proximosObstaculosValidos :: Int -> (Terreno, [Obstaculo]) -> [Obstaculo]
 proximosObstaculosValidos _ (Rio _, []) = [Nenhum,Tronco]
 proximosObstaculosValidos _ (Relva, []) = [Nenhum,Arvore]
-proximosObstaculosValidos _ (Estrada, []) = [Nenhum,Carro]
+proximosObstaculosValidos _ (Estrada vel, []) = [Nenhum,Carro]
 proximosObstaculosValidos n (te, (x:xs)) | obsemlinha (Mapa n (te, (x:xs))) && n > length (x:xs) && isRio (te, (x:xs)) = [Nenhum,Tronco] 
                                          | obsemlinha (Mapa n (te, (x:xs))) && n > length (x:xs) &&  isRelva' = [Nenhum, Arvore]
                                          | obsemlinha (Mapa n (te, (x:xs))) && 
