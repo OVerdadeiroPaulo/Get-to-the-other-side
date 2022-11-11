8e4d422a64d4cf980eae2187de001433f63e0490
paulo moreira <a64459@alunos.uminho.pt>
2022-11-11 13:02:29 +0000

    .

---
+++ src/codigodelixo.hs
@@ -0,0 +1,63 @@
+module Lixo where
+import LI12223 
+import LI12223 (Terreno(Relva))
+{-import LI12223 (Terreno(Rio, Estrada, Relva))
+deslocaobs :: Mapa -> Coordenadas -> Obstaculo
+deslocaobs mapa@(Mapa l (((Rio vel, (h:t)):xs))) (a,b)
+  |a == 0 = veobstaculonacoordenada (mapa) (l-(vel-1),b)
+  | a == l = veobstaculonacoordenada mapa (0+(vel-1),b)
+  |otherwise = veobstaculonacoordenada mapa (a+vel,b)
+deslocaobs mapa@(Mapa l (((Estrada vel, (h:t)):xs))) (a,b)
+  |a == 0 = veobstaculonacoordenada (mapa) (l-(vel-1),b)
+  | a == l = veobstaculonacoordenada mapa (0+(vel-1),b)
+  |otherwise = veobstaculonacoordenada mapa (a+vel,b)
+deslocaobs mapa@(Mapa l ([(Relva, obs)])) (a,b) = veobstaculonacoordenada mapa (a,b)
+
+-}
+
+{-|funcao  que valida se na ha demasiados terrenos do mesmo tipo-}
+{-terrenosseguidos :: Int -> Mapa -> Bool
+terrenosseguidos _ (Mapa _ ([])) = True
+terrenosseguidos 4 (Mapa l (((Rio _, _):xs))) = False
+terrenosseguidos k (Mapa l (((Rio _, _):(xa:xs)))) =
+     case xa of 
+      (Rio _, _) -> terrenosseguidos (k+1) (Mapa l ((xa:xs)))
+      _ -> terrenosseguidos 0 (Mapa l ((xa:xs)))
+terrenosseguidos 5 (Mapa l (((Estrada _, _):xs))) = False
+terrenosseguidos k (Mapa l (((Estrada _, _):xa:xs))) =
+     case xa of 
+      (Estrada _, _) -> terrenosseguidos (k+1) (Mapa l ((xa:xs)))
+      _ -> terrenosseguidos 0 (Mapa l ((xa:xs)))
+terrenosseguidos 5 (Mapa l (((Relva, _):xs))) = False
+terrenosseguidos k (Mapa l (((Relva, _):xa:xs))) =
+     case xa of 
+      (Relva, _) -> terrenosseguidos (k+1) (Mapa l ((xa:xs)))
+      _ -> terrenosseguidos 0 (Mapa l ((xa:xs)))
+
+tipodeobsaux :: Mapa -> Bool
+tipodeobsaux (Mapa larg (((_, []) :y))) = True
+tipodeobsaux (Mapa larg (((Relva, (x:xs)):ys)))
+  | x==Carro ||x==Tronco = False
+  | otherwise = tipodeobsaux (Mapa larg (((Relva, (xs)):ys)))
+tipodeobsaux (Mapa larg (((Rio vel, (x:xs)):ys)))
+  | x==Carro ||x==Arvore = False
+  |otherwise = tipodeobsaux (Mapa larg (((Rio vel, (xs)):ys)))
+tipodeobsaux (Mapa larg (((Estrada vel, (x:xs)):ys)))
+  | x==Tronco || x==Arvore = False
+  |otherwise = tipodeobsaux (Mapa larg (((Estrada vel, (xs)):ys)))
+-}
+inicio :: Show a => a -> [Char]
+inicio x =(take 3(show x))
+depara :: Terreno -> String
+depara (Rio vel) = "Rio"
+depara (Estrada  vel) = "Estrada" 
+depara Relva =  "Relva"
+--  deriving (Show(Int-> Mapa))
+obsnaonenhum :: Terreno -> Obstaculo
+obsnaonenhum (Rio vel) = Tronco
+obsnaonenhum (Estrada vel) = Carro
+obsnaonenhum (Relva) = Arvore
+proxobsval :: (Terreno,[Obstaculo]) -> [Obstaculo]
+proxobsval par@(terr,[]) = [Nenhum, (obsnaonenhum terr)]
+proxobsval par@(terr,(o:bs)) 
+  | [Nenhum,(obsnaonenhum terr)]
